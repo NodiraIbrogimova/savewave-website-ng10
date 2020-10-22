e066b04ad0030c9b8f67b0aef40655c6d631c629
Nodira <nodira.ibrogimova@gmail.com>
2020-10-22 17:26:24 +0500

    perf: build project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './parts/header/header.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
+import { JoinComponent } from './pages/join/join.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DiscoverComponent,
+    JoinComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
